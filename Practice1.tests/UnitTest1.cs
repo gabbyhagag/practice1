a62aaf5e810c421616ec64b7320a8d079aa86685
Elad Ben Shmuel <elad.bash@gmail.com>
2018-04-06 23:54:32 +0300

    more unit tests

--- Practice1.tests/UnitTest1.cs
+++ Practice1.tests/UnitTest1.cs
@@ -40,5 +40,168 @@ namespace Practice1.tests
             //assert
             Assert.AreEqual(result, expected);
         }
+
+        [TestMethod]
+        public void TestMaxWithMaxToSmallest()
+        {
+            //arrange
+            Stack1 stack = new Stack1();
+            int expected = 10;
+            int result;
+
+            //act
+            stack.Push(10);
+            stack.Push(5);
+            stack.Push(1);
+            result = stack.Max();
+
+            //assert
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void TestMaxInMiddleWithPops()
+        {
+            //arrange
+            Stack1 stack = new Stack1();
+            int[] values = new int[] {1,2,10,5,7};
+            int expected = 10;
+            int result;
+
+            //act
+            foreach(int val in values)
+            {
+                stack.Push(val);
+            }
+            stack.Pop();
+            stack.Pop();
+
+            result = stack.Max();
+
+            //assert
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void TestMaxWhenWasAndPoped()
+        {
+            //arrange
+            Stack1 stack = new Stack1();
+            int[] values = new int[] { 1, 2, 10, 5, 20 };
+            int expected = 10;
+            int result;
+
+            //act
+            foreach (int val in values)
+            {
+                stack.Push(val);
+            }
+            stack.Pop();
+            stack.Pop();
+
+            result = stack.Max();
+
+            //assert
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void TestMaxWithDoubleValues()
+        {
+            //arrange
+            Stack1 stack = new Stack1();
+            int[] values = new int[] { 1, 2, 10, 10, 11 };
+            int expected = 10;
+            int result;
+
+            //act
+            foreach (int val in values)
+            {
+                stack.Push(val);
+            }
+            stack.Pop();
+            stack.Pop();
+
+            result = stack.Max();
+
+            //assert
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void TestMaxWithDoubleValuesPoped()
+        {
+            //arrange
+            Stack1 stack = new Stack1();
+            int[] values = new int[] { 1, 2, 10, 10, 11 };
+            int expected = 2;
+            int result;
+
+            //act
+            foreach (int val in values)
+            {
+                stack.Push(val);
+            }
+            stack.Pop();
+            stack.Pop();
+            stack.Pop();
+
+            result = stack.Max();
+
+            //assert
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void TestMaxWithGaps()
+        {
+            //arrange
+            Stack1 stack = new Stack1();
+            int[] values = new int[] { 1, 10, 2, 10, 11 };
+            int expected = 10;
+            int result;
+
+            //act
+            foreach (int val in values)
+            {
+                stack.Push(val);
+            }
+            stack.Pop();
+            stack.Pop();
+
+            result = stack.Max();
+
+            //assert
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void PushPopRangeTest()
+        {
+            //arrange
+            Stack1 stack = new Stack1();
+            int result;
+
+            stack.Push(10);
+            Assert.AreEqual(stack.Max(), 10, "Value should be 10");
+
+            stack.Push(15);
+            Assert.AreEqual(stack.Max(), 15, "Value should be 15");
+
+            stack.Push(3);
+            Assert.AreEqual(stack.Max(), 15, "Value should be 15");
+
+            stack.Push(100);
+            Assert.AreEqual(stack.Max(), 100, "Value should be 100");
+
+            stack.Pop();
+            Assert.AreEqual(stack.Max(), 15, "Value should be 15");
+
+            stack.Pop();
+            Assert.AreEqual(stack.Max(), 15, "Value should be 15");
+
+            stack.Pop();
+            Assert.AreEqual(stack.Max(), 10, "Value should be 10");
+        }
     }
 }
